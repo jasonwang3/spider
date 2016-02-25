a75267470630562038f1ffc66988c9830e2d51fe
jason <jason.wang@everbridge.com>
2016-02-25 18:50:20 +0800

    update selector

--- spider-downloader/src/main/scala/com/spider/selector/AbstractSelectable.scala
+++ spider-downloader/src/main/scala/com/spider/selector/AbstractSelectable.scala
@@ -3,6 +3,22 @@ package com.spider.selector
 /**
   * Created by jason on 16-2-2.
   */
-abstract class AbstractSelectable extends Selectable{
+abstract class AbstractSelectable extends Selectable {
+
+  protected def getSourceTexts: List[String]
+
+  override def css(selector: String): Selectable = {
+    $(selector)
+  }
+
+  override def css(selector: String, attrName: String): Selectable = {
+    $(selector, attrName)
+  }
+
+//  protected def select(selector: Selector, strings: List[String]): Selectable = {
+//    for (string <- strings;
+//         result = selector.select(string);
+//         if (result != null)) yield result
+//  }
 
 }
---
+++ spider-downloader/src/main/scala/com/spider/selector/Selector.scala
@@ -0,0 +1,10 @@
+package com.spider.selector
+
+/**
+  * Created by jason on 16-2-25.
+  */
+trait Selector {
+  def select(text: String): String
+
+  def selectList(test: String): List[String];
+}
