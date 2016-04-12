c01542d7525065b5c6178ccfb7769aa0a5216e40
jason <jason.wang@everbridge.com>
2016-04-12 17:46:02 +0800

    add test case for OA

--- spider-downloader/src/main/scala/com/spider/downloader/actor/DownloadCoordinatorActor.scala
+++ spider-downloader/src/main/scala/com/spider/downloader/actor/DownloadCoordinatorActor.scala
@@ -6,6 +6,7 @@ import akka.event.Logging
 import com.spider.core.akka.spring.SpringServiceHelper
 import com.spider.downloader.{AbstractDownloader, HttpClientDownloader}
 import com.spider.model.downloader.DownloadRequest
+import com.spider.selector.impl.Page
 
 /**
   * Created by jason on 16-3-15.
@@ -16,12 +17,18 @@ class DownloadCoordinatorActor extends Actor {
 
   override def receive: Receive = {
     case downloadRequest: DownloadRequest => processRequest(downloadRequest)
+    case page: Page => processPage(page)
     case _ => log.info("received unknown message")
   }
 
   def processRequest(downloadRequest: DownloadRequest) = {
 
   }
+
+  def processPage(page: Page) = {
+
+  }
+
 
   override def preStart() = {
     downloader = SpringServiceHelper.getBean("httpClientDownloader").asInstanceOf[HttpClientDownloader]
--- spider-downloader/src/test/scala/downloader/HttpClientDownloaderTest.scala
+++ spider-downloader/src/test/scala/downloader/HttpClientDownloaderTest.scala
@@ -12,8 +12,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext
   * Created by wxy on 16/2/21.
   */
 
-class HttpClientDownloaderTest extends FlatSpec with Matchers with BeforeAndAfter{
-  var applicationContext:ApplicationContext = null
+class HttpClientDownloaderTest extends FlatSpec with Matchers with BeforeAndAfter {
+  var applicationContext: ApplicationContext = null
   var httpClientDownloader: HttpClientDownloader = null
 
   before {
@@ -24,7 +24,7 @@ class HttpClientDownloaderTest extends FlatSpec with Matchers with BeforeAndAfte
 
   "download" should "download a page" in {
     val html = httpClientDownloader.download("https://github.com")
-    html.getFirstSourceText.isEmpty should be (false)
+    html.getFirstSourceText.isEmpty should be(false)
   }
 
   "download with cookies" should "download a page" in {
@@ -34,8 +34,15 @@ class HttpClientDownloaderTest extends FlatSpec with Matchers with BeforeAndAfte
       .addCookie("github.com", "tz", "Asia%2FShanghai")
       .addCookie("github.com", "user_session", "nW84V1XqEMGOU0ZwCFw19lAULBCSdNMDXztlcpdIESwGqCbOVwYXyadqNNa9vG6snuxlzNjmigc9WFHw")
     val page = httpClientDownloader.download(new Request("https://github.com"), site.toTask())
-    page.getHtml.getFirstSourceText.isEmpty should be (false)
+    page.getHtml.getFirstSourceText.isEmpty should be(false)
   }
 
+  "download http://219.238.188.179/" should "download a page" in {
+    val site: Site = Site.create().setDomain("219.238.188.179").addCookie("219.238.188.179", "JSESSIONID", "D9656D8AC0FD9151C0666D5CDEBB94C1")
+      .addCookie("219.238.188.179", "login.locale", "zh_CN")
+      .addHeader("User-Agent","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36")
+    val page = httpClientDownloader.download(new Request("http://219.238.188.179/seeyon/main.do?main.do?app=Coll&method=morePending4App&_spage=&page=2&count=125&pageSize=20"), site.toTask())
+    println(page.getHtml.getDocument)
+  }
 
 }
